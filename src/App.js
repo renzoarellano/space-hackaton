0bd338720ddc4628a7eb45e933cd3df631ea56e4
Walter Renzo Muñoz Arellano <wmunozarellano@gmail.com>
2021-04-14 13:19:27 -0500

    [TEST] Netlify deploy

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
-import Home from "./pages/Home";
+/* import Home from "./pages/Home"; */
 import Login from "./pages/Login";
 import Error from "./pages/Error";
 import ScrollToTop from "./components/common/ScrollToTop";
